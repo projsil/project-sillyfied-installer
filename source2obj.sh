38bdf3f7a6d2299c7b8eec93da5afbb7a179ff03
Sillyfications! <ubiplayergt@gmail.com>
2026-06-01 11:41:59 +0200

    switch to cmake, add gitignore for build folder

--- source2obj.sh
+++
@@ -1,6 +0,0 @@
-for f in projsilinstaller-win/*.cpp; do
-  out=build/obj/$(basename "${f%.*}").o
-  g++ -std=c++17 -fPIC -Wall -Wextra -O2 \
-    -Iprojsilinstaller-win -I./vcpkg_installed/x64-linux/include \
-    -c "$f" -o "$out"
-done
